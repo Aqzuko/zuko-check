e75bf3123831f94d357e6f272ebf6767c099ab2c
Aqzuko <connerkneisel@gmail.com>
2025-06-25 01:42:52 -0500

    Update niggatech.ps1

--- niggatech.ps1
+++ niggatech.ps1
@@ -1,7 +1,1 @@
-function Format-Output {
-    param($name, $value)
-    $output = "{0} : {1}" -f $name, $value -replace 'System.Byte\[\]', ''
-    if ($output -notmatch "Steam|Origin|EAPlay|FileSyncConfig.exe|OutlookForWindows") {
-        return $output
-    }
-}
+
